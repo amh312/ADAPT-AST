8a9382749ccbfb95792ca37b024d42b87823e5db
amh312 <127774411+amh312@users.noreply.github.com>
2023-06-23 10:55:25 +0100

    first working version

--- ADAPT_AST_1.1.R
+++ ADAPT_AST_1.1.R
@@ -16,25 +16,25 @@ load("~/Documents/Projects/PhD/ADAPT-AST/ABX.RData")
 
 
 AAST1_input <- te_amr_uti[1,]
+AAST1_group <- AAST1_input$age_group
+ar_SXT <- subset(SXT_model_df, select = colnames(SXT_model_df) == paste("ar_SXT[",AAST1_group,"]", sep=""))
+br_SXT <- subset(SXT_model_df, select = grepl(paste("br_SXT\\[", AAST1_group,",", sep = ""), colnames(SXT_model_df)))
+B_SXT <- subset(SXT_model_df, select = grepl(paste("B_SXT", sep = ""), colnames(SXT_model_df)))
+AAST1_x_SXT <- as.matrix(model.matrix( SXT ~ B1_SXT +
+                                          B2_SXT +
+                                          B3_SXT +
+                                          B4_SXT +
+                                          B5_SXT ,
+                                          data = AAST1_input ))
+AAST1_x_SXT <- AAST1_x_SXT[,2:ncol(AAST1_x_SXT)]
+AAST1_input <- as.data.frame(AAST1_input)
 
 # GENERATE PREDICTIONS
   
 ## SXT
-  
-log_prob <- sapply(AAST_input, function(x)
-  SXT_model_df$a1_SXT + SXT_model_df$ar_SXT[AAST1_input$te_age_group_SXT[n]] +
-    sum(te_x_SXT[n,] * (SXT_model_df$br_SXT[AAST1_input$te_age_group_SXT[n],] +
-                          SXT_model_df$B_SXT)))
-  
 
+log_prob <- SXT_model_df$a1_SXT + ar_SXT +
+    rowSums(AAST1_x_SXT * (br_SXT + B_SXT))
+SXT_y_pr <- 1 / (1 + exp(-log_prob))
 
-
-view(log_prob)
-
-SXT_y_pr[n] <- exp(log_prob) / (1 + exp(log_prob))
-
-SXT_model_df <- as.data.frame(SXT_model)
-
-view(AAST1_input)
-
-ncol(SXT_model_df)
+HPDI(SXT_y_pr)
